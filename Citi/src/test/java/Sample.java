82a1918acd353c0ba80f558538ba5741ef485026
saafin <saafin@gmail.com>
2014-08-18 16:43:14 -0400

    Initial commit

---
+++ Citi/src/test/java/Sample.java
@@ -0,0 +1,11 @@
+import org.testng.annotations.Test;
+
+/**
+ * Created by Saafiin on 8/18/2014.
+ */
+public class Sample extends Common{
+
+    @Test
+    printmyName("gags");
+
+}
---
+++ Generic/src/test/java/Common.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Saafiin on 8/18/2014.
+ */
+public class Common  {
+    public void printMyname (String name){
+        System.out.println(name);
+    }
+}
